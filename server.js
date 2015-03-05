daf595e7b52f49337be58e4d14d23c9630756bb5
tmathews <thomas.c.mathews@gmail.com>
2015-03-04 20:17:39 -0800

    Added random room redirection.

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require("express")
 var jade    = require("jade")
+var random  = require("random-word")
 
 var app = express()
 app.enable("trust proxy")
 app.use(express.static("./client/bin"))
 app.use(express.static("./client/assets"))
 
-app.get(/^[^.]*$/, function (req, res) {
+// Redirects client to random game room.
+app.get(/^\/$/, function (req, res){
+  res.redirect("/" + random())
+})
+
+// Opens game room.
+app.get(/^\/[^.\/]+\/?$/, function (req, res) {
   jade.renderFile("./html.jade", {}, function(err, html) {
     if (!err) return res.send(html)
     res.status(503).send("Could not render the page.")
