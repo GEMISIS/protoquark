e6078ead525f9f94701f504c0a06caf0dab8c06a
tmathews <thomas.c.mathews@gmail.com>
2015-03-05 21:24:53 -0800

    Added chat module.

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 var Router = require("./router")
+var Chat   = require("./chat")
 
 function onRoom (name) {
   console.log("connect to or create room:", name)
@@ -8,8 +9,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
   // Main entry point.
   console.log("Hello World.")
 
+  var el = document.body
+
   var router = new Router()
   router.add("room", /^\/([^\/]+)\/?$/)
   router.on("route:room", onRoom)
   router.listen()
+
+  var chat = new Chat
+  el.appendChild(chat.el)
+
+  window.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) chat.focus()
+  })
 })
