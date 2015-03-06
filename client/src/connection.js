98c1e2021725fda126536b7e1339728810ba5c6f
lostlevels <lostlevels@users.noreply.github.com>
2015-03-05 20:36:46 -0800

    refactor connection

--- client/src/connection.js
+++ client/src/connection.js
@@ -1,87 +1,88 @@
 var API_KEY = "lwjd5qra8257b9"
 
-function Connection(id, server) {
-  if (server) {
-    console.log("Serving ...")
-    this.serve(id)
-  }
-  else {
-    console.log("Connecting ...")
-    this.connect(id)
+function Connection(room) {
+  this.room = room
+  this.connect()
+}
+
+Connection.prototype = {
+  send: function send(data, receiver) {
+    var clients = this.clients
+      , clientToServerConn = this.clientToServerConn
+
+    if (receiver && clients && clients[receiver]) {
+      clients[receiver].send(data)
+    }
+    else if (clientToServerConn) {
+      clientToServerConn.send(data)
+    }
+  },
+
+  connect: function connect() {
+    var peer = this.peer = new Peer({key : API_KEY})
+    peer.on("error", onJoinError.bind(this))
+    peer.on("open", onClientIdAssigned.bind(this))
+
+    var clientToServerConn = this.clientToServerConn = peer.connect(this.room)
+    clientToServerConn.on("open", onConnectedToServer.bind(this))
+    clientToServerConn.on("data", onServerData.bind(this))
   }
 }
 
-Connection.prototype.connect = function(serverId) {
-
-  var self = this
-  this.peer = new Peer({key : API_KEY})
-
-  this.peer.on("error", function(err) {
-    console.log("error " + err)
-
-    // Unable to connect to server, become server ourself
-    console.log("No server found, creating one...")
-    self.serve(serverId)
-  })
-
-  this.peer.on("open", function(id) {
-    console.log("you are", id)
-    self.id = id
-  })
-
-  this.clientToServerConn = this.peer.connect(serverId)
-
-  this.clientToServerConn.on("error", function(err) {
-    console.log("error " + err)
-  })
-
-  this.clientToServerConn.on("open", function() {
-    console.log("Connected to server")
-  })
-
-  this.clientToServerConn.on("data", function(data) {
-    console.log("Server data", data)
-  })
-}
-
-Connection.prototype.serve = function(id) {
-  var self = this
-  
-  this.peer = new Peer(id, {key : API_KEY})
+function onClientIdAssigned(id) {
+  console.log("You are", id)
   this.id = id
+}
 
+function onConnectedToServer() {
+  console.log("Connected to server")
+}
+
+function onJoinError(err) {
+  console.log("Unable to join room, starting up new server", err)
+
+  // Kill any connections so that client doesn't end up joining to self
+  this.peer.disconnect()
+
+  serve.call(this)
+}
+
+function onServerData(data) {
+  console.log("Received data from server", data)
+}
+
+function onClientData(data) {
+  console.log("Received client data", data)
+}
+
+function onClientDisconnected() {
+  var conn = arguments[0]
+  delete this.clients[conn.peer]
+  console.log("User closed", conn)
+}
+
+function onClientConnected(conn) {
+  console.log("Client connected ", conn.peer, conn.id)
+
+  this.clients[conn.peer] = conn
+
+  conn.on("data", onClientData.bind(this))
+  conn.on("close", onClientDisconnected.bind(this, conn))
+}
+
+function onServerStarted() {
+  console.log("server started")
+  this.id = this.room
+}
+
+function serve() {
   // In case this was previously a client, delete the client to host connection
   delete this.clientToServerConn
-
   this.clients = {}
 
-  this.peer.on("connection", function(conn) {
-    console.log("Peer connected ", conn.id, conn.label, conn.peer)
-
-    self.clients[conn.peer] = conn
-
-    conn.on("data", function(data) {
-      console.log("Received from client " + data)
-    })
-
-    conn.on("close", function() {
-      delete self.clients[conn.peer]
-      console.log("User closed")
-    })
-  })
-
-  this.peer.on("open", function(id) {
-    console.log("server opened")
-  })
-}
-
-Connection.prototype.send = function(data, receiver) {
-  if (this.clientToServerConn) {
-    this.clientToServerConn.send(data)
-  }
-  else if (receiver && this.clients && this.clients[receiver]) {
-    this.clients[receiver].send(data)
-  }
+  var peer = this.peer = new Peer(this.room, {key : API_KEY})
+  peer.on("connection", onClientConnected.bind(this))
+  peer.on("open", onServerStarted.bind(this))
 }
 
 module.exports = Connection
