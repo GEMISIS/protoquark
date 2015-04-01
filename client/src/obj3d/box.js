df54296a4aa930347a9470207bc4364537407f8e
tmathews <thomas.c.mathews@gmail.com>
2015-03-31 19:49:29 -0700

    Added box file.

---
+++ client/src/obj3d/box.js
@@ -0,0 +1,27 @@
+var geometry;
+function getGeometry() {
+  if (!geometry) geometry = new THREE.BoxGeometry(1, 1, 1)
+  return geometry
+}
+
+function Box (entity, color) {
+  this.entity = entity
+  this.o3d = new THREE.Object3D()
+
+  var material = new THREE.MeshBasicMaterial({
+    color: color
+  })
+
+  this.o3d.add(new THREE.Mesh(getGeometry(), material))
+}
+
+Box.prototype = {
+  update: function update (dt) {
+    var o3d = this.o3d
+    var e = this.entity
+    o3d.position.copy(e.position)
+    o3d.rotation.setFromQuaternion(e.rotation)
+  }
+}
+
+module.exports = Box
