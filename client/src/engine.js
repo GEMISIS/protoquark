aa1860e57fc4b9984a099c63b1bd2deffb0b3e3e
lostlevels <lostlevels@users.noreply.github.com>
2015-06-05 17:19:50 -0700

    remove reliable to see if lag dissapears

--- client/src/engine.js
+++ client/src/engine.js
@@ -444,10 +444,7 @@ function onStateSend() {
   })
 
   if (states.length) {
-    conn.send("gamestate", {
-      states: states
-    },
-    {reliable: true})
+    conn.send("gamestate", { states: states })
   }
 }
 
